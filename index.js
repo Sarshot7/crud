ddbc77cbc87c00a57e41ed6aa4c963aca3c19c66
Sarshot7 <sarshot7@gmail.com>
2022-10-08 01:20:56 +0530

    crud done

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3000
 
 
-mongoose.connect("mongodb+srv://sarshot77:776atlas@cluster0.trc860d.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://sarshot_db:776atlaS@cluster0.k07ck2i.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://sarshot77:776atlas@cluster0.trc860d.mongodb.net/?retryWrites=true&w=majority")
+
 .then(()=>{
     console.log('connected')
 }).catch((error)=>{
@@ -31,7 +32,7 @@ app.post('/users', async(req,res)=>{
 })
 
 
-app.get('/users',async(req,res)=>{
+app.get('/users', async (req,res)=>{
     try{
         const data = await Users.find();
         res.json(data)
@@ -40,6 +41,43 @@ app.get('/users',async(req,res)=>{
         console.log("error")
     }
 })
+app.get('/user/:id', async (req,res)=>{
+    try{
+        const _id = req.params.id;
+        const data = await Users.findById(_id);
+        res.json(data)
+    }
+    catch(error){
+        console.log("error")
+    }
+})
+app.delete('/user/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const data = await Users.findByIdAndDelete( _id)
+        res.send(`Document with ${data.title} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+app.put('/user/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Users.findByIdAndUpdate(
+            _id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
 app.listen(port,()=>{
     console.log("started");
 })
